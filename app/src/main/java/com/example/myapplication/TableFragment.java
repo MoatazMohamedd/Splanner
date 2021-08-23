6f8b0d8a49be255e159eedcc9ad90e75fd6ad0c5
MoatazMohamedd <moataz.mohamed@ymail.com>
2021-08-23 03:36:02 +0200

    Update TableFragment.java

--- app/src/main/java/com/example/myapplication/TableFragment.java
+++ app/src/main/java/com/example/myapplication/TableFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
 
 public class TableFragment extends Fragment {
 
 
-
+    private GridLayout gridLayout;
+    private TextView   subjectCell;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,7 +27,23 @@ public class TableFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_table, container, false);
+        View view= inflater.inflate(R.layout.fragment_table, container, false);
+
+        // To access a certain view in fragment
+        // we need the big view itself which is represented
+        // in the view variable above and access all the views
+        // from within this view by view.findViewById ...
+
+        gridLayout= view.findViewById(R.id.gridLayout);
+        subjectCell= (TextView) gridLayout.getChildAt(5);
+
+       if(subjectCell.getText().equals(""))
+        {
+            subjectCell.setBackgroundColor(Color.GREEN);
+        }
+
+        return  view;
     }
 }
